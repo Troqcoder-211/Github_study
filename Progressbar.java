8ae0dbca182205476dbb32d38950910a5948e5f7
Nguyen Thanh Trong <thanhtrong@mail.com>
2024-03-18 23:54:12 +0700

    add jframe for progressbar

--- Progressbar.java
+++ Progressbar.java
@@ -1,4 +1,6 @@
-public class Progressbar {
+import javax.swing.JFrame;
+
+public class Progressbar extends JFrame {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
